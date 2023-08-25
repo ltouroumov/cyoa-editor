c993685d206cae0f0075409beaa62433051b9c14
Laureline David <ltouroumov@gmail.com>
2023-08-25 22:43:16 +0200

    Default Files List

--- pages/viewer.vue
+++ pages/viewer.vue
@@ -5,19 +5,53 @@
   </div>
   <div v-else class="dialog-container">
     <div class="dialog">
+      <h5>Default Files</h5>
+      <ul class="list-group mb-3">
+        <li class="list-group-item">
+          <a
+            href="https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/project-v17.json"
+            @click.prevent="loadRemoteFile"
+          >
+            Worm V6 (Lt's Fork)
+          </a>
+        </li>
+      </ul>
+
+      <div
+        v-if="isLoading"
+        class="d-flex align-items-center justify-content-start gap-3 mb-3"
+      >
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <strong>Loading ...</strong>
+      </div>
+
+      <h5>Load File</h5>
       <LoadProject />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-
+import { ref } from '#imports';
 import ScoreBar from '~/components/viewer/ScoreBar.vue';
-import { useProjectStore } from '~/composables/store/project';
+import { useProjectRefs, useProjectStore } from '~/composables/store/project';
 
-const store = useProjectStore();
-const { project } = storeToRefs(store);
+const { loadProject } = useProjectStore();
+const { project } = useProjectRefs();
+
+const isLoading = ref<boolean>(false);
+
+const loadRemoteFile = async ({ target }: MouseEvent) => {
+  if (target && target instanceof HTMLAnchorElement) {
+    isLoading.value = true;
+    const fileURL = target.href;
+    const result: any = await $fetch(fileURL, { parseResponse: JSON.parse });
+    loadProject(result, fileURL);
+    isLoading.value = false;
+  }
+};
 </script>
 
 <style lang="scss">
