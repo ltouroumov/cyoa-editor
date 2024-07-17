9449aeda7c26462d24ed28dee9d84719983254bb
Iris Hoffmeyer <hoffmeyer25@gmail.com>
2024-07-17 18:57:34 -0400

    Add a menu to swap projects
    It probably could use work style wise (eg centering) but I don't really feel like figuring that out.

---
+++ components/viewer/modal/ViewProjectMenu.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="dialog-container">
+    <div class="dialog bg-dark-subtle text-light">
+      <h5>Default Files</h5>
+      <ul class="list-group mb-3">
+        <li
+          v-for="projects in projectList.items"
+          :key="projects.title"
+          class="list-group-item"
+        >
+          <a
+            href="#"
+            :data-fileurl="projects.remoteFileUrl"
+            @click.prevent="loadRemoteFile"
+          >
+            {{ projects.title }}
+          </a>
+        </li>
+      </ul>
+      <div
+        v-if="isLoading"
+        class="d-flex align-items-center justify-content-start gap-3 mb-3"
+      >
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <strong>Loading ...</strong>
+      </div>
+
+      <h5>Load File</h5>
+      <LoadProject />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ViewerProjectList } from 'composables/viewer';
+import { useProjectRefs, useProjectStore } from '~/composables/store/project';
+
+const isLoading = ref<boolean>(false);
+
+const { projectList } = defineProps<{
+  projectList: ViewerProjectList;
+}>();
+
+const { loadProject, unloadProject } = useProjectStore();
+const { isLoaded } = useProjectRefs();
+
+const loadRemoteFile = async ({ target }: MouseEvent) => {
+  if (target && target instanceof HTMLAnchorElement) {
+    const fileURL = target.dataset.fileurl;
+    if (!fileURL) return;
+    isLoading.value = true;
+    const result: any = await $fetch(fileURL, { parseResponse: JSON.parse });
+    if (!isLoaded.value) {
+      loadProject(result, fileURL);
+    } else {
+      unloadProject();
+      // Wait for the unloadProject to finish before loading the new project
+      nextTick(() => {
+        if (!isLoaded.value) {
+          loadProject(result, fileURL);
+        }
+      });
+    }
+    isLoading.value = false;
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/css/bootstrap/config';
+</style>
