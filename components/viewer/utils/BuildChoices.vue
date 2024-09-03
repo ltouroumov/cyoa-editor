884fc575b4da7782f0723a04069bfd20b3607f83
Laureline Valérie David <ltouroumov@gmail.com>
2024-09-03 17:16:37 +0200

    Format the saved build list

--- components/viewer/utils/BuildChoices.vue
+++ components/viewer/utils/BuildChoices.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="choices-list">
     <template v-for="({ packRow, choices }, idx) in packRows" :key="packRow.id">
-      <span v-if="idx > 0" class="spacer">|</span>
+      <span v-if="idx > 0" class="group-spacer">|</span>
       <strong class="choice-group-title">{{ packRow.title }}:</strong>
-      <span v-for="{ obj, count } in choices" :key="obj.id" class="choice-name">
-        {{ obj.title }}
-        <span v-if="obj.isSelectableMultiple" class="choice-count"
-          >(x{{ count }})</span
-        >
-      </span>
+      <template v-for="({ obj, count }, idx) in choices" :key="obj.id">
+        <span class="choice-name">
+          {{ obj.title }}
+          <span v-if="obj.isSelectableMultiple" class="choice-count">
+            (x{{ count }})
+          </span>
+        </span>
+        <span v-if="idx < choices.length - 1" class="choice-spacer">,</span>
+      </template>
     </template>
   </div>
 </template>
@@ -69,7 +72,15 @@ const packRows = computed(() => {
   padding-top: 0.5rem;
   border-top: 1px solid var(--bs-border-color);
 
-  .spacer {
+  .choice-group-title {
+    margin-right: 0.2rem;
+  }
+
+  .choice-spacer {
+    margin-right: 0.2rem;
+  }
+
+  .group-spacer {
     margin-left: 0.2rem;
     margin-right: 0.2rem;
   }
