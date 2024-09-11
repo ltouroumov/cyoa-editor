92f6d07a20213144f1a86356e242f87452cd45fc
Iris Hoffmeyer <hoffmeyer25@gmail.com>
2024-09-11 00:01:50 -0400

    Add backpack downloading Inital attempt

--- components/viewer/modal/BackpackModal.vue
+++ components/viewer/modal/BackpackModal.vue
@@ -5,56 +5,75 @@
     </template>
     <template #default>
       <div class="pack-content flex-grow-1 bg-dark">
-        <div class="pack-info-container">
-          <div class="pack-scores">
-            <ViewScoreStatus vertical />
-          </div>
-          <div class="d-flex flex-column pack-selection-controls">
-            <div class="form-check form-switch">
-              <input
-                id="packRowDisabledSwitch"
-                v-model="lockBackpackObjects"
-                class="form-check-input"
-                type="checkbox"
-                role="switch"
-              />
-              <label class="form-check-label" for="packRowDisabledSwitch">
-                Lock Objects in the Backpack
-              </label>
-            </div>
-            <div class="form-check form-switch">
-              <input
-                id="hideDisabledAddons"
-                v-model="hideDisabledAddons"
-                class="form-check-input"
-                type="checkbox"
-                role="switch"
-              />
-              <label class="form-check-label" for="hideDisabledAddons">
-                Hide Disabled Addons
-              </label>
-            </div>
-          </div>
-        </div>
+        <button type="button" class="btn btn-primary" @click="backpackToImage">
+          Download backpack as Image
+        </button>
         <div
-          v-for="{ packRow, choices } in packRows"
-          :key="packRow.id"
-          class="pack-row"
+          v-if="isLoading"
+          class="d-flex align-items-center justify-content-start mb-3 gap-3"
         >
-          <div class="pack-row-title">
-            {{ packRow.title }}
+          <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
           </div>
-          <div class="container-fluid p-0">
-            <div class="row g-2">
-              <ViewProjectObj
-                v-for="{ obj, row } in choices"
-                :key="obj.id"
-                :obj="obj"
-                :row="row"
-                :width="packRow.objectWidth"
-                :view-object="objectMode"
-                :hide-disabled-addons="hideDisabledAddons"
-              />
+          <strong>{{
+            !progress
+              ? 'Generating image... (This might take a while)'
+              : progress
+          }}</strong>
+        </div>
+
+        <div ref="backpackRef" class="backpack-container">
+          <div class="pack-info-container">
+            <div class="pack-scores">
+              <ViewScoreStatus vertical />
+            </div>
+            <div class="d-flex flex-column pack-selection-controls">
+              <div class="form-check form-switch">
+                <input
+                  id="packRowDisabledSwitch"
+                  v-model="lockBackpackObjects"
+                  class="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                />
+                <label class="form-check-label" for="packRowDisabledSwitch">
+                  Lock Objects in the Backpack
+                </label>
+              </div>
+              <div class="form-check form-switch">
+                <input
+                  id="hideDisabledAddons"
+                  v-model="hideDisabledAddons"
+                  class="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                />
+                <label class="form-check-label" for="hideDisabledAddons">
+                  Hide Disabled Addons
+                </label>
+              </div>
+            </div>
+          </div>
+          <div
+            v-for="{ packRow, choices } in packRows"
+            :key="packRow.id"
+            class="pack-row"
+          >
+            <div class="pack-row-title">
+              {{ packRow.title }}
+            </div>
+            <div class="container-fluid p-0">
+              <div class="row g-2">
+                <ViewProjectObj
+                  v-for="{ obj, row } in choices"
+                  :key="obj.id"
+                  :obj="obj"
+                  :row="row"
+                  :width="packRow.objectWidth"
+                  :view-object="objectMode"
+                  :hide-disabled-addons="hideDisabledAddons"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -68,6 +87,7 @@
 </template>
 
 <script setup lang="ts">
+import { domToDataUrl } from 'modern-screenshot';
 import * as R from 'ramda';
 import { computed } from 'vue';
 
@@ -79,7 +99,7 @@ import { useProjectRefs, useProjectStore } from '~/composables/store/project';
 import { useViewerRefs, useViewerStore } from '~/composables/store/viewer';
 import { ViewContext } from '~/composables/viewer';
 
-const { getObject, getObjectRow, getRow } = useProjectStore();
+const { getObject, getObjectRow, getRow, project } = useProjectStore();
 const { selected, backpack } = useProjectRefs();
 const { toggleBackpack } = useViewerStore();
 const { isBackpackVisible } = useViewerRefs();
@@ -114,6 +134,43 @@ const objectMode = computed(() => {
   if (lockBackpackObjects.value) return ViewContext.BackpackDisabled;
   else return ViewContext.BackpackEnabled;
 });
+
+const backpackRef = ref<HTMLDivElement>();
+const isLoading = ref(false);
+const progress = ref<string | null>(null);
+const backpackToImage = async () => {
+  if (backpackRef.value && packRows.value.length >= 1) {
+    isLoading.value = true;
+    // A hack, the DOM won't update until after html2canvas is called otherwise
+    const pause = new Promise((resolve) => setTimeout(resolve, 100));
+    await pause;
+    const url = await domToDataUrl(backpackRef.value, {
+      backgroundColor: project?.data.styling.backgroundColor,
+      async progress(current: number, total: number) {
+        await nextTick(() => {
+          progress.value = `Downloading images... ${Math.round(
+            (current / total) * 100,
+          )}%`;
+        });
+      },
+    });
+    progress.value = null;
+    isLoading.value = false;
+    const element = document.createElement('a');
+    element.href = url;
+    element.download = `backpack-${new Date().toLocaleString()}.png`;
+
+    await nextTick(() => {
+      element.click();
+    });
+
+    URL.revokeObjectURL(url);
+  } else if (packRows.value.length === 0) {
+    alert(
+      'No objects selected to create a backpack image, select at least one object to create a image.',
+    );
+  }
+};
 </script>
 
 <style scoped lang="scss">
