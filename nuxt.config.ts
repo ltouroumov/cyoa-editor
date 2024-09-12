1fe09ae7c86ba64bd66ae585260224f073cdd61a
Iris Hoffmeyer <hoffmeyer25@gmail.com>
2024-09-12 17:39:13 -0400

    Add favicon's
    This is 100% inspired by me getting annoyed by the error generated in console.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,48 @@ export default defineNuxtConfig({
     buildAssetsDir: 'assets',
     head: {
       title: 'Interactive CYOA',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'favicon.ico',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: 'favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: 'favicon-16x16.png',
+        },
+        {
+          rel: 'android-chrome',
+          type: 'image/png',
+          sizes: '92x192',
+          href: 'android-chrome-92x192.png',
+        },
+        {
+          rel: 'andriod-chrome',
+          type: 'image/png',
+          sizes: '512x512',
+          href: 'android-chrome-512x512.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '180x180',
+          href: 'apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' },
+      ],
     },
   },
 
