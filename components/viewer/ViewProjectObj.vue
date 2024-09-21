2a9aba4ba2cb141612afe20d2f9363d7aab0cf95
Iris Hoffmeyer <hoffmeyer25@gmail.com>
2024-09-21 12:42:13 -0400

    Update components/viewer/ViewProjectObj.vue
    Co-authored-by: Laureline David <ltouroumov@gmail.com>

--- components/viewer/ViewProjectObj.vue
+++ components/viewer/ViewProjectObj.vue
@@ -135,7 +135,8 @@ const isEnabled = computed<boolean>(() => {
   // Whether the object is always enabled or disabled based on the viewObject
   // Otherwise check the object conditions
   switch ($props.viewObject) {
-    case ViewContext.BackpackEnabled || ViewContext.BackpackDisabled:
+    case ViewContext.BackpackEnabled:
+    case ViewContext.BackpackDisabled:
       return true;
     default:
       return condition.value(selectedIds.value);
