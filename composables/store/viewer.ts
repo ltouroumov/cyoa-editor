ff562332dead84f4e0ddbbd9db1636b642130686
Iris Hoffmeyer <hoffmeyer25@gmail.com>
2024-10-01 17:57:14 -0400

    Persistant storage of viewer settings

--- composables/store/viewer.ts
+++ composables/store/viewer.ts
@@ -2,65 +2,74 @@ import { defineStore, storeToRefs } from 'pinia';
 
 import type { ViewerProjectList } from '~/composables/viewer';
 
-export const useViewerStore = defineStore('viewer', () => {
-  const isBackpackVisible = ref<boolean>(false);
-  const isSearchVisible = ref<boolean>(false);
-  const isProjectMenuVisible = ref<boolean>(false);
-  const hideDisabledAddons = ref<boolean>(false);
+export const useViewerStore = defineStore(
+  'viewer',
+  () => {
+    const isBackpackVisible = ref<boolean>(false);
+    const isSearchVisible = ref<boolean>(false);
+    const isProjectMenuVisible = ref<boolean>(false);
+    const hideDisabledAddons = ref<boolean>(false);
 
-  const viewerProjectList = ref<ViewerProjectList>({
-    items: [
-      {
-        remoteFileUrl:
-          'https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/extract-v6.0.json',
-        title: "Worm V6.0 (Pixel's Version)",
-      },
-      {
-        remoteFileUrl:
-          'https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/extract-v6.1.json',
-        title: "Worm V6.1 (Pixel's Version)",
-      },
-      {
-        remoteFileUrl:
-          'https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/project-v17.json',
-        title: "Worm V6 (Lt's Fork)",
-      },
-      {
-        remoteFileUrl:
-          'https://raw.githubusercontent.com/ltouroumov/pathfinder-cyoa/main/project-v0.json',
-        title: 'Pathfinder CYOA (WIP by Lt Ouroumov)',
-      },
-      // Add more projects...
-    ],
-  });
+    const viewerProjectList = ref<ViewerProjectList>({
+      items: [
+        {
+          remoteFileUrl:
+            'https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/extract-v6.0.json',
+          title: "Worm V6.0 (Pixel's Version)",
+        },
+        {
+          remoteFileUrl:
+            'https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/extract-v6.1.json',
+          title: "Worm V6.1 (Pixel's Version)",
+        },
+        {
+          remoteFileUrl:
+            'https://raw.githubusercontent.com/ltouroumov/worm-cyoa-v6-fork/master/project-v17.json',
+          title: "Worm V6 (Lt's Fork)",
+        },
+        {
+          remoteFileUrl:
+            'https://raw.githubusercontent.com/ltouroumov/pathfinder-cyoa/main/project-v0.json',
+          title: 'Pathfinder CYOA (WIP by Lt Ouroumov)',
+        },
+        // Add more projects...
+      ],
+    });
 
-  const toggleBackpack = (set?: boolean) => {
-    isBackpackVisible.value = set ?? !isBackpackVisible.value;
-  };
+    const toggleBackpack = (set?: boolean) => {
+      isBackpackVisible.value = set ?? !isBackpackVisible.value;
+    };
 
-  const toggleSearch = (set?: boolean) => {
-    isSearchVisible.value = set ?? !isSearchVisible.value;
-  };
+    const toggleSearch = (set?: boolean) => {
+      isSearchVisible.value = set ?? !isSearchVisible.value;
+    };
 
-  const toggleProjectMenu = (set?: boolean) => {
-    isProjectMenuVisible.value = set ?? !isProjectMenuVisible.value;
-  };
+    const toggleProjectMenu = (set?: boolean) => {
+      isProjectMenuVisible.value = set ?? !isProjectMenuVisible.value;
+    };
 
-  const toggleHideDisabledAddons = (set?: boolean) => {
-    hideDisabledAddons.value = set ?? !hideDisabledAddons.value;
-  };
+    const toggleHideDisabledAddons = (set?: boolean) => {
+      hideDisabledAddons.value = set ?? !hideDisabledAddons.value;
+    };
 
-  return {
-    isBackpackVisible,
-    isSearchVisible,
-    isProjectMenuVisible,
-    hideDisabledAddons,
-    viewerProjectList,
-    toggleBackpack,
-    toggleSearch,
-    toggleProjectMenu,
-    toggleHideDisabledAddons,
-  };
-});
+    return {
+      isBackpackVisible,
+      isSearchVisible,
+      isProjectMenuVisible,
+      hideDisabledAddons,
+      viewerProjectList,
+      toggleBackpack,
+      toggleSearch,
+      toggleProjectMenu,
+      toggleHideDisabledAddons,
+    };
+  },
+  {
+    persist: {
+      // Only save the `hideDisabledAddons` state
+      pick: ['hideDisabledAddons'],
+    },
+  },
+);
 
 export const useViewerRefs = () => storeToRefs(useViewerStore());
