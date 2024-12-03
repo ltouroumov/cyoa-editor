15408c58b277f8338975bb00296ed0f5122c6335
Laureline Valérie David <ltouroumov@gmail.com>
2024-12-03 17:53:41 +0100

    WIP

--- pages/editor.vue
+++ pages/editor.vue
@@ -7,12 +7,23 @@
   </div>
   <div v-if="isNotNil(project)" class="container-fluid">
     <div class="row p-1">
-      <Menubar />
+      <Menubar :model="menu">
+        <template #end>
+          <div class="flex flex-row items-center">
+            <div class="flex flex-row items-center">
+              <label class="me-2">Preview</label>
+              <ToggleSwitch v-model="preview" size="small" variant="outlined" />
+            </div>
+            <Divider layout="vertical" />
+            <h1 class="text-amber-500 text-xl">
+              {{ project.projectName }}
+            </h1>
+          </div>
+        </template>
+      </Menubar>
     </div>
-    Project Loaded
-    <div v-for="row in project.data.rows" :key="row.id">
-      <div>{{ row.title }}</div>
-    </div>
+    <div v-if="preview">PREVIEW</div>
+    <div v-if="!preview">EDITOR</div>
   </div>
   <DynamicDialog />
 </template>
@@ -20,9 +31,62 @@
 <script setup lang="ts">
 import { isNotNil } from 'ramda';
 
-import { useProjectRefs } from '~/composables/store/project';
+import { useProjectRefs, useProjectStore } from '~/composables/store/project';
 
+const { unloadProject } = useProjectStore();
 const { store, project } = useProjectRefs();
+
+const preview = ref<boolean>(false);
+const menu = [
+  {
+    label: 'Project',
+    icon: 'iconify solar--file-bold-duotone',
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star',
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-search',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt',
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server',
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil',
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette',
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope',
+  },
+];
+
+const doUnload = () => {
+  unloadProject();
+};
 </script>
 
 <style>
