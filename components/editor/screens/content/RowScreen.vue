532e3debe9850ef9d61ef8d1f33a6334c9ce4fc9
Laureline Valérie David <ltouroumov@gmail.com>
2024-12-24 17:11:42 +0100

    WIP Editor

--- components/editor/screens/content/RowScreen.vue
+++ components/editor/screens/content/RowScreen.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="flex flex-col gap-2">
-    <div>Props ...</div>
+    <Fluid>
+      <div class="flex flex-col gap-2 justify-stretch">
+        <div class="flex flex-row gap-2">
+          <IftaLabel class="grow">
+            <InputText v-model="row.name" />
+            <label class="font-bold" for="rowName">Name</label>
+          </IftaLabel>
+          <IftaLabel>
+            <InputText v-model="row.id" disabled />
+            <label class="font-bold" for="rowName">ID</label>
+          </IftaLabel>
+        </div>
+
+        <div v-if="row.header" class="flex flex-col gap-2">
+          <h3 class="font-bold text-xl">Header</h3>
+          <IftaLabel>
+            <InputText v-model="row.header.title" />
+            <label class="font-bold" for="rowName">Title</label>
+          </IftaLabel>
+          <IftaLabel>
+            <Textarea v-model="row.header.text" class="min-h-[10rem]" />
+            <label class="font-bold" for="rowName">Text</label>
+          </IftaLabel>
+        </div>
+      </div>
+    </Fluid>
   </div>
   <DataView
     :value="children"
