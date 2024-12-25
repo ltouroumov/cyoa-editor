eeb10c9a5d1615a905e0c39b1017b1ddd826f4b9
Laureline Valérie David <ltouroumov@gmail.com>
2024-12-25 13:08:30 +0100

    WIP Editor

---
+++ components/editor/screens/content/RowImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-row justify-center items-center bg-surface-700">
+    <Image :src="media.data" pt:image:class="object-contain object-center" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ProjectImage } from '~/composables/project/types/v2/media';
+import { useProjectStore } from '~/composables/project/useProjectStore';
+
+const projectStore = useProjectStore();
+const props = defineProps<{
+  mediaId: string;
+}>();
+
+const media = computed((): ProjectImage => {
+  return projectStore.media.images[props.mediaId];
+});
+</script>
+
+<style scoped lang="scss"></style>
--- components/editor/screens/content/RowScreenHeader.vue
+++ components/editor/screens/content/RowScreenHeader.vue
@@ -13,16 +13,67 @@
           </IftaLabel>
         </div>
 
-        <div v-if="row.header" class="flex flex-col gap-2">
-          <h3 class="font-bold text-xl">Header</h3>
-          <IftaLabel>
-            <InputText v-model="row.header.title" />
-            <label class="font-bold" for="rowName">Title</label>
-          </IftaLabel>
-          <IftaLabel>
-            <Textarea v-model="row.header.text" class="min-h-[10rem]" />
-            <label class="font-bold" for="rowName">Text</label>
-          </IftaLabel>
+        <div class="grid grid-cols-4 gap-2">
+          <div v-if="row.header" class="flex flex-col gap-2 col-span-2">
+            <div class="flex flex-row justify-between">
+              <h3 class="font-bold text-xl">Header</h3>
+              <IconButton
+                outlined
+                severity="danger"
+                icon="iconify solar--trash-bin-trash-line-duotone"
+                size="small"
+              />
+            </div>
+            <div class="grid grid-cols-3 gap-2">
+              <div
+                class="flex flex-col gap-2 items-stretch justify-center grow"
+              >
+                <div
+                  class="flex flex-row justify-stretch grow relative group rounded overflow-auto"
+                >
+                  <Skeleton
+                    v-if="isNil(row.header?.image)"
+                    animation="none"
+                    class="grow"
+                    height="auto"
+                  />
+                  <RowImage
+                    v-if="row.header.image"
+                    :media-id="row.header.image"
+                    class="grow border border-surface-700"
+                  />
+                  <div
+                    class="absolute bg-slate-500/50 top-0 left-0 right-0 bottom-0 opacity-0 group-hover:opacity-100 duration-300 flex flex-row items-center justify-center cursor-pointer"
+                  >
+                    <i class="iconify solar--pen-line-duotone" />
+                    <IconButton
+                      v-if="row.header.image"
+                      outlined
+                      severity="danger"
+                      icon="iconify solar--trash-bin-trash-line-duotone"
+                      class="absolute right-[0.5rem] bottom-[0.5rem]"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-col gap-2 col-span-2">
+                <IftaLabel>
+                  <InputText v-model="row.header.title" />
+                  <label class="font-bold" for="rowName">Title</label>
+                </IftaLabel>
+                <IftaLabel>
+                  <Textarea v-model="row.header.text" class="min-h-[10rem]" />
+                  <label class="font-bold" for="rowName">Text</label>
+                </IftaLabel>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2 grow">
+            <h3 class="font-bold text-xl">Layout</h3>
+          </div>
+          <div class="flex flex-col gap-2 grow">
+            <h3 class="font-bold text-xl">Conditions</h3>
+          </div>
         </div>
       </div>
     </Fluid>
@@ -30,6 +81,9 @@
 </template>
 
 <script setup lang="ts">
+import { isNil } from 'ramda';
+
+import RowImage from '~/components/editor/screens/content/RowImage.vue';
 import { ObjectType } from '~/composables/project/types/v2/objects/base';
 import { useProjectStore } from '~/composables/project/useProjectStore';
 
