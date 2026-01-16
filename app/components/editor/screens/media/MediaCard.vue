284ad4e7d43f62b117e6a81bcc55835ec8a3e938
Laureline David <ltouroumov@gmail.com>
2026-01-16 12:38:23 +0100

    media screen

---
+++ app/components/editor/screens/media/MediaCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="border border-surface-500 rounded overflow-hidden flex flex-col hover:border-primary transition-colors cursor-pointer"
+  >
+    <div class="aspect-video bg-surface-100 relative overflow-hidden">
+      <img
+        v-if="image.isRemote"
+        :src="image.data"
+        :alt="image.id"
+        class="w-full h-full object-cover"
+      />
+      <img
+        v-else
+        :src="`data:image/png;base64,${image.data}`"
+        :alt="image.id"
+        class="w-full h-full object-cover"
+      />
+      <div
+        class="absolute bottom-0 left-0 right-0 bg-black/70 text-white text-xs font-mono px-2 py-1 truncate"
+      >
+        {{ image.id }}
+      </div>
+    </div>
+    <div class="p-2 flex flex-row gap-2 justify-end">
+      <Button size="small" variant="outlined" severity="secondary">
+        Edit
+      </Button>
+      <Button size="small" variant="outlined" severity="danger">
+        Delete
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ProjectImage } from '~/composables/project/types/v2/media';
+
+const props = defineProps<{
+  image: ProjectImage;
+}>();
+</script>
+
+<style scoped lang="scss"></style>
--- app/components/editor/screens/media/MediaScreen.vue
+++ app/components/editor/screens/media/MediaScreen.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>Media</div>
+  <DataView :value="images" data-key="id">
+    <template #list="{ items }">
+      <div class="grid grid-cols-3 gap-2">
+        <div
+          v-for="item in items"
+          :key="item.id"
+          class="col-span-3 md:col-span-1"
+        >
+          <MediaCard :image="item" />
+        </div>
+      </div>
+    </template>
+  </DataView>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { useProjectStoreRefs } from '~/composables/project/useProjectStore';
+
+const { media } = useProjectStoreRefs();
+
+const images = computed(() => {
+  return Object.values(media.value.images);
+});
+</script>
+
 <style scoped lang="scss"></style>
