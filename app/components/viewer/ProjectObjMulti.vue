5d577719b3d1228a7feb5f0a36f5b90a259da97f
Laureline David <ltouroumov@gmail.com>
2026-02-03 08:43:45 +0100

    multi-select: change appearance

--- app/components/viewer/ProjectObjMulti.vue
+++ app/components/viewer/ProjectObjMulti.vue
@@ -1,59 +1,55 @@
 <template>
   <div class="obj-select-multi">
-    <div
-      class="flex flex-row items-stretch gap-1 w-full mx-2 px-2 py-0 relative"
-      :class="{
-        'p-inputtext': mode === 'edit',
-        'border border-transparent': mode === 'show',
-      }"
-    >
-      <div
-        v-if="canToggle"
-        class="btn-decrement"
-        :class="{
-          'text-green-400': selectedAmount > minSelectedAmount,
-          'text-gray-400': selectedAmount <= minSelectedAmount,
-        }"
-        @click="decrement"
-      >
-        <i class="pi pi-minus" />
-      </div>
-      <div
-        v-if="mode === 'show'"
-        class="grow flex flex-row gap-2 items-center relative"
-        @click.stop.prevent="mode = 'edit'"
-      >
-        <span class="grow text-center">{{ selectedAmount }}</span>
-      </div>
-      <div v-if="mode === 'edit'" class="grow flex flex-row gap-1 relative">
-        <input
-          type="text"
-          v-model="amountInput"
-          class="w-full focus:outline-none text-center"
-        />
+    <div class="flex flex-row w-full mx-2 px-2 py-0 relative">
+      <div class="flex flex-row items-stretch gap-1 grow relative z-10">
         <div
-          class="absolute right-0 top-0 bottom-0 flex flex-row justify-center items-center"
+          v-if="canToggle"
+          class="btn-decrement"
+          :class="{
+            'text-green-400': selectedAmount > minSelectedAmount,
+            'text-gray-400': selectedAmount <= minSelectedAmount,
+          }"
+          @click="decrement"
         >
-          <div
-            class="pi pi-check size-5 my-2 text-gray-200 shrink-0 cursor-pointer"
-            @click="mode = 'show'"
+          <i class="pi pi-minus" />
+        </div>
+        <div
+          v-if="mode === 'show'"
+          class="grow flex flex-row gap-2 items-center relative"
+          @click.stop.prevent="mode = 'edit'"
+        >
+          <span class="grow text-center">{{ selectedAmount }}</span>
+        </div>
+        <div v-if="mode === 'edit'" class="grow flex flex-row gap-1 relative">
+          <input
+            type="text"
+            v-model="amountInput"
+            class="w-full focus:outline-none text-center"
           />
+          <div
+            class="absolute right-0 top-0 bottom-0 flex flex-row justify-center items-center"
+          >
+            <div
+              class="pi pi-check size-5 my-2 text-gray-200 shrink-0 cursor-pointer"
+              @click="mode = 'show'"
+            />
+          </div>
+        </div>
+        <div
+          v-if="canToggle"
+          class="btn-increment"
+          :class="{
+            'text-green-400': selectedAmount < maxSelectedAmount,
+            'text-gray-400': selectedAmount >= minSelectedAmount,
+          }"
+          @click="increment"
+        >
+          <i class="pi pi-plus" />
         </div>
       </div>
       <div
-        v-if="canToggle"
-        class="btn-increment"
-        :class="{
-          'text-green-400': selectedAmount < maxSelectedAmount,
-          'text-gray-400': selectedAmount >= minSelectedAmount,
-        }"
-        @click="increment"
-      >
-        <i class="pi pi-plus" />
-      </div>
-      <div
         v-if="mode === 'edit'"
-        class="absolute right-0 left-0 top-[45px] flex flex-col px-2 py-1 rounded-md bg-surface-600/30 backdrop-blur-md z-10"
+        class="absolute right-0 left-0 top-0 pt-10 flex flex-col px-2 py-1 p-inputtext z-5"
       >
         <div class="h-6 flex flex-row items-center px-2">
           <Slider
