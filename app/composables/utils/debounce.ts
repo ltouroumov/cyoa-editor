8e016a0bc73ed732593662486317fd7f9c333979
Laureline David <ltouroumov@gmail.com>
2026-05-12 14:36:05 +0200

    feat: extend debounce utility with cancel and flush methods

--- app/composables/utils/debounce.ts
+++ app/composables/utils/debounce.ts
@@ -1,11 +1,41 @@
+export interface DebouncedFn<Args extends Array<any>> {
+  (...args: Args): void;
+  cancel(): void;
+  flush(): void;
+}
+
 export function debounce<Args extends Array<any>>(
   fn: (...args: Args) => void,
   delay: number,
-): (...args: Args) => void {
-  let nextTimeoutId: number | undefined;
+): DebouncedFn<Args> {
+  let timeoutId: number | undefined;
+  let lastArgs: Args | undefined;
 
-  return (...args: Args) => {
-    window.clearTimeout(nextTimeoutId);
-    nextTimeoutId = window.setTimeout(() => fn(...args), delay);
+  const call = (...args: Args): void => {
+    lastArgs = args;
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      fn(...args);
+      lastArgs = undefined;
+      timeoutId = undefined;
+    }, delay);
   };
+
+  call.cancel = (): void => {
+    window.clearTimeout(timeoutId);
+    timeoutId = undefined;
+    lastArgs = undefined;
+  };
+
+  call.flush = (): void => {
+    if (lastArgs !== undefined) {
+      window.clearTimeout(timeoutId);
+      const args = lastArgs;
+      lastArgs = undefined;
+      timeoutId = undefined;
+      fn(...args);
+    }
+  };
+
+  return call;
 }
